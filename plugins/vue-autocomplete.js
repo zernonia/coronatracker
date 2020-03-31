67f5edf530987e792280c42ee8a64a990a6926d3
zernonia <59365435+zernonia@users.noreply.github.com>
2020-04-01 01:40:51 +0800

    Trying to fix PurgeCSS

--- plugins/vue-autocomplete.js
+++ plugins/vue-autocomplete.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Autocomplete from '@trevoreyre/autocomplete-vue'
+/* purgecss start ignore */
 import '@trevoreyre/autocomplete-vue/dist/style.css'
+/* purgecss end ignore */
 
 Vue.use(Autocomplete)
